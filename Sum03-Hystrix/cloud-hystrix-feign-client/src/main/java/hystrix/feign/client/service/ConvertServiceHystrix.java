e3bc463ce4c55718048b061aa0a7b8310d547194
zqzhou2 <598106158@qq.com>
2017-06-06 20:52:43 +0800

    断路器

--- Sum03-Hystrix/cloud-hystrix-feign-client/src/main/java/hystrix/feign/client/service/ConvertServiceHystrix.java
+++ Sum03-Hystrix/cloud-hystrix-feign-client/src/main/java/hystrix/feign/client/service/ConvertServiceHystrix.java
@@ -1,10 +1,14 @@
 package hystrix.feign.client.service;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by zqzhou2 on 2017/6/6.
  */
+@Component
 public class ConvertServiceHystrix implements ConvertService {
 
     @Override
